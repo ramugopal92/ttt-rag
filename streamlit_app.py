c5a3b44b53077ac8f80c17e7e4a3a3a67c007971
Ramu G <190281896+ramugopal92@users.noreply.github.com>
2026-01-29 01:49:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,21 @@ from rag_engine import answer_question
 st.set_page_config(page_title="The Tech Thinker AI", page_icon="logo.png", layout="centered")
 
 # Branding Header
-st.image("logo.png", width=45)  
-st.title("The Tech Thinker AI")
-st.caption("Powered by The Tech Thinker • RAG + GPT-4o-mini")
+# ========= Inline Logo + Title =========
+col1, col2 = st.columns([1, 6])  # adjust ratio if needed
+
+with col1:
+    st.image("logo.png", width=55)  # your logo
+
+with col2:
+    st.markdown(
+        """
+        <h1 style='margin-bottom:0px;'>The Tech Thinker AI</h1>
+        <p style='margin-top:-10px; color:gray;'>Powered by The Tech Thinker • RAG + GPT-4o-mini</p>
+        """,
+        unsafe_allow_html=True
+    )
+
 
 
 if "messages" not in st.session_state:
